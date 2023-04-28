a420265fd0fcb1c1eed2dd588af95f9a128b5d31
Loadingzzz <privetpokanet@gmail.com>
2023-04-28 23:10:18 +0300

    frontend: v1.3.3

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,9 +1,9 @@
 import "./index.css";
-import Home from "./Components/Home/Home";
-import Header from "./Components/Header/Header";
-import Login from "./Pages/Login/Login";
-import Registration from "./Pages/Registration/Registration";
-import PostPage from "./Components/PostPage/PostPage";
+import Home from "./Components/Home/Home.jsx";
+import Header from "./Components/Header/Header.jsx";
+import Login from "./Pages/Login/Login.jsx";
+import Registration from "./Pages/Registration/Registration.jsx";
+import PostPage from "./Components/PostPage/PostPage.jsx";
 
 import { Container } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <Container maxWidth="lg">
         <Routes>
-          {/* <Route path="/ANCS/" element={<Home />} /> */}
+          <Route path="/ANCS/" element={<Home />} />
           <Route path="/ANCS/login" element={<Login />} />
           <Route path="/ANCS/registration" element={<Registration />} />
           <Route path="/ANCS/post:id" element={<PostPage />} />
