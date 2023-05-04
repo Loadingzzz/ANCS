f7312982855778830db942ab8bc381d0c4087eee
Loadingzzz <privetpokanet@gmail.com>
2023-05-04 10:05:45 +0300

    frontend: v1.4.1

--- frontend/components/Home/Home.jsx
+++ frontend/components/Home/Home.jsx
@@ -1,26 +1,36 @@
 import React, { useEffect } from "react";
 import Post from "../Post/Post.jsx";
-import { Button, Grid } from "@mui/material";
-import { Link, json } from "react-router-dom";
-import axios from "../../axios/axios.js";
+// import { Button, Grid } from "@mui/material";
+// import { Link, json } from "react-router-dom";
+// import axios from "../../axios/axios.js";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../../redux/Slice/post.js";
+import styles from "./Home.module.scss";
+import { Tab, Tabs } from "@mui/material";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   const { posts, tags } = useSelector((state) => state.posts);
 
-  const isPostsLoading = posts.status === "loading";
+  const isPostsLoading = posts.status === "loading" || "loaded";
 
   useEffect(() => {
     dispatch(fetchPosts());
   }, []);
-
+  const value = {};
   return (
-    <div>
+    <div className={styles.wrapper}>
+      <Tabs
+        style={{ marginBottom: 15 }}
+        value={0}
+        aria-label="basic tabs example"
+      >
+        <Tab label="Новые" />
+        <Tab label="Популярные" />
+      </Tabs>
       {isPostsLoading ? (
-        <Post />
+        <Post title="Default post" text="Default post text" />
       ) : (
         posts.items.map((obj, index) => {
           return (
--- frontend/components/Post/Post.jsx
+++ frontend/components/Post/Post.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import styles from "./Post.module.scss";
 import { Link } from "react-router-dom";
+import { IconButton, Tooltip } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
 
 const Post = (props) => {
   return (
     <div className={styles.post}>
-      <Link to={"/ANCS/post:id"} className={styles.post__title}>
-        {props.title}
-      </Link>
+      <div className={styles.post__top}>
+        <Link to={"/ANCS/post:id"} className={styles.post__title}>
+          {props.title}
+        </Link>
+        <div>
+          <Tooltip title="create">
+            <IconButton>
+              <BorderColorIcon color="disabled" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="delete">
+            <IconButton>
+              <DeleteIcon color="disabled" />
+            </IconButton>
+          </Tooltip>
+        </div>
+      </div>
       <p className={styles.post__text}>{props.text}</p>
       <div className={styles.post__imgage__wrapper}>
         {props.image ? (
